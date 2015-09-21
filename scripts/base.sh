72632656d4622ab23d9fb3e7d363df73220934b5
Mai Krakauer <mkrakauer@covario.com>
2015-09-21 10:09:50 -0700

    Add ssh2 libraries

--- scripts/base.sh
+++ scripts/base.sh
@@ -5,13 +5,21 @@ yum -y update
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
 yum -y install httpd php56w php56w-opcache php56w-gd php56w-intl php56w-mbstring php56w-mysql php56w-pdo php56w-mcrypt php56w-pecl-memcached php56w-xml php56w-devel php56w-pecl-xdebug ph56w-soap
-yum -y install gcc wget screen nodejs openssl-devel mod_ssl memcached subversion git perl-DBD-MySQL facter vim-enhanced vim-common
+yum -y install gcc wget screen nodejs openssl-devel libssh2 libssh2-devel mod_ssl memcached subversion git perl-DBD-MySQL facter vim-enhanced vim-common kernel-devel dos2unix
 
 # php mongo drivers
 if [ ! -f "/etc/php.d/mongodb.ini" ]; then
     printf "\n" | pecl install mongo
     cat > /etc/php.d/mongodb.ini << EOF
 extension=mongo.so
+EOF
+fi
+
+# php ssh2 drivers
+if [ ! -f "/etc/php.d/ssh2.ini" ]; then
+    printf "\n" | pecl install ssh2-0.12
+    cat > /etc/php.d/ssh2.ini << EOF
+extension=ssh2.so
 EOF
 fi
 
@@ -32,6 +40,5 @@ fi
 # configure daemons startup modes
 chkconfig httpd on
 chkconfig memcached on
-chkconfig mailhog on
 chkconfig iptables off
 chkconfig ip6tables off
